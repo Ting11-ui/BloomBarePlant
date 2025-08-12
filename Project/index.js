6d770e2c13d55d0f7799c1594d1da35aef788b26
PHE RITHIKA <pherithika@gmail.com>
2025-08-13 04:45:24 +0700

    Second commit

---
+++ Project/index.js
@@ -0,0 +1,70 @@
+const header = [
+    "HOME",
+    "PLANT",
+    "ABOUT",
+    "CONTACT US"
+]
+
+
+const menu = document.getElementById('menu');
+const cancel = document.getElementById('cancel');
+const offcanvas = document.getElementById('offcanvas');
+const imgLeaf = document.getElementById('imgLeaf');
+const rightSide = document.getElementById('rightSide');
+
+
+//offcanvas 
+menu.onclick = () =>{
+    offcanvas.classList.remove('-translate-x-full');
+    offcanvas.classList.add('translate-x-0');
+}
+
+cancel.onclick = () =>{
+    offcanvas.classList.remove('translate-x-0');
+    offcanvas.classList.add('-translate-x-full');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//header left side
+// const menu = [
+//     'HOME' ,'PLANTS' ,'ABOUT' , 'CONTACT' ,
+// ]
+
+// menu.map((elements, index) => {
+//     const span = document.createElement('span');
+//     span.textContent= elements;
+//     span.className='textHeader cursor-pointer text-2xl font-bold inline-block';
+
+//     rightSide.appendChild(span);
+// })
+
